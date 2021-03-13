73781621a4cfd003e567034fc052a52318bb171d
AdelNoroozi <adel13no@gmail.com>
2021-03-13 15:46:35 +0330

    FINAL edit

---
+++ README.md
@@ -0,0 +1,7 @@
+# project-team07
+# Members
+
+Parsa Farahani 992023024 -
+Adel Noroozi 992023036 -
+Pouya Alavi 992023023 -
+Hamed Khosravi 992023010
---
+++ src/main/java/config/DatabaseConfig.java
@@ -0,0 +1,7 @@
+package config;
+
+public class DatabaseConfig {
+    public static final String CONNECTION_URL = "jdbc:mysql://127.0.0.1:3306/bombullet" ;
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "nokia5233irms";
+}
