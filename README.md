7006cc5fb57c7ebbeeae4a7084ad876025f9fe4c
AdelNoroozi <adel13no@gmail.com>
2021-03-13 14:32:09 +0330

    final phase01 changes

--- README.md
+++
@@ -1,7 +0,0 @@
-# project-team07
-# Members
-
-Parsa Farahani 992023024 -
-Adel Noroozi 992023036 -
-Pouya Alavi 992023023 -
-Hamed Khosravi 992023010
--- src/main/java/config/DatabaseConfig.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-public class DatabaseConfig {
-    public static final String CONNECTION_URL = "jdbc:mysql://127.0.0.1:3306/bonbullet" ;
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "nokia5233irms";
-}
