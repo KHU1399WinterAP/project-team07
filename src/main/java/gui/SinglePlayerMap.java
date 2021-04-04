f0e41c0bc149dd5409e611c0888277b3a04be4c5
PouyaALV81 <pouya.platiniumz@gmail.com>
2021-04-05 00:02:51 +0430

    Recource is fixed

--- src/main/java/gui/SinglePlayerMap.java
+++ src/main/java/gui/SinglePlayerMap.java
@@ -1904,6 +1904,7 @@ public class SinglePlayerMap extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+System.out.println("Hi");
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
